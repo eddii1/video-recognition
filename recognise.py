24a0b426700b4c798d90d52f35744a4a16980d7a
Edi <tudoranedi@gmail.com>
2024-12-22 14:47:11 +0200

    PRIMUL UNGHI CALCULAT - LIVE

--- recognise.py
+++ recognise.py
@@ -1,5 +1,30 @@
 import cv2
 import mediapipe as mp
+import math
+
+def calculare_unghi(a, b, c):
+
+    #cream vectorii ab , bc
+    ab = (a[0] - b[0], a[1] - b[1])
+    bc = (c[0] - b[0], c[1] - b[1])
+
+    #produs scalar
+    prod_scalar = ab[0] * bc[0] + ab[1] * bc[1]
+
+    #modulul vectorilor
+    modul_ab = math.sqrt(ab[0] ** 2 + ab[1] ** 2)
+    modul_bc = math.sqrt(bc[0] ** 2 + bc[1] ** 2)
+
+    if modul_ab == 0 or modul_bc == 0:
+        return 0.0
+    
+    cos_unghi = prod_scalar / (modul_ab * modul_bc)
+    cos_unghi = max(-1.0, min(1.0, cos_unghi))
+
+    unghi = math.degrees(math.acos(cos_unghi))
+    return unghi
+
+
 
 cap = cv2.VideoCapture("test2.mp4")
 ret, frame = cap.read()
@@ -36,27 +61,30 @@ else:
                 resized_frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS
             )
 
-        umar_stang = results.pose_landmarks.landmark[11]
+            landmarks = results.pose_landmarks.landmark
 
-        x_pixel = int(umar_stang.x * new_width)
-        y_pixel = int(umar_stang.y * new_height)
+            umar = (landmarks[11].x * new_width, landmarks[11].y * new_height)
+            cot = (landmarks[13].x * new_width, landmarks[13].y * new_height)
+            incheietura = (landmarks[15].x * new_width, landmarks[15].y * new_height)
 
-        cv2.putText(
-            resized_frame,                      # Image to draw on
-            f"Umar Stang: (x:{x_pixel}, y:{y_pixel})",                      # Text
-            (50, 100),                  # Bottom-left corner of the text
-            cv2.FONT_HERSHEY_COMPLEX,   # Font type
-            1.5,                        # Font scale
-            (255, 255, 255),            # Color (white)
-            3,                          # Thickness
-            cv2.LINE_AA                 # Line type (anti-aliased)
-        )
+            unghi = calculare_unghi (umar, cot, incheietura) 
 
-        cv2.circle(resized_frame, (x_pixel, y_pixel), 15, (0,255,0), -1)
+            cv2.putText(
+                resized_frame,                      # Image to draw on
+                f"Unghi: {int(unghi)}",                      # Text
+                (50, 100),                  # Bottom-left corner of the text
+                cv2.FONT_HERSHEY_COMPLEX,   # Font type
+                1.5,                        # Font scale
+                (255, 255, 255),            # Color (white)
+                3,                          # Thickness
+                cv2.LINE_AA                 # Line type (anti-aliased)
+            )
+
+            # cv2.circle(resized_frame, (x_pixel, y_pixel), 15, (0,255,0), 1)
 
         cv2.imshow("Algoritm Recunoastere Video", resized_frame)
 
-        if cv2.waitKey(30) == 27:
+        if cv2.waitKey(30) == 27 or cv2.waitKey(30) == 113:
             break
 
 cap.release()
